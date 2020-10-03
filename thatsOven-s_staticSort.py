cf8ba62b0a0a6edde641f935ff111331795afb56
thatsOven <68780017+thatsOven@users.noreply.github.com>
2020-10-03 10:03:54 +0200

    staticSort is now fully in-place

--- thatsOven-s_staticSort.py
+++ thatsOven-s_staticSort.py
@@ -1,40 +1,47 @@
+# 2020 Copyright(C) thatsOven
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+def insertion_sort(InputList, n):
+    for i in range(1, n):
+        j = i-1
+        nxt_element = InputList[i]
+        while (InputList[j] > nxt_element) and (j >= 0):
+            InputList[j+1] = InputList[j]
+            j=j-1
+        InputList[j+1] = nxt_element
+from itertools import chain
 def thatsOvens_staticSort(a):
     M = max(a)
     size = len(a)
-    if M > size:
-        for _ in range(M - size + 1):
-            a.insert(0, [])
-    if M == size:
-        a.insert(0, [])
+    constant = M/(size+4)
     counter = 0
     listcount = 0
     while listcount < size:
         if type(a[counter]) is list:
             counter += 1
         else:
-            if int(a[counter]) == counter:
+            if int(a[counter]*constant) == counter:
                 a[counter] = [a[counter]]
             else:
-                if type(a[a[counter]]) is list:
-                    a[a[counter]].append(a[counter])
+                if type(a[int(a[counter]*constant)]) is list:
+                    a[int(a[counter]*constant)].append(a[counter])
                     a[counter] = []
                 else:
-                    a[a[counter]], a[counter] = [a[counter]], a[a[counter]]
+                    a[int(a[counter]*constant)], a[counter] = [a[counter]], a[int(a[counter]*constant)]
             listcount += 1 
-    size = len(a)
-    counter = 0
-    for _ in range(size):
-        l = len(a[counter])
-        if l == 0:
-            a.remove(a[counter])
-            size -= 1
-        elif l == 1:
-            a[counter] = a[counter][0]
-            counter += 1
-        else:
-            temp = a[counter].copy()
-            a.remove(a[counter])
-            lt = len(temp)
-            for j in range(lt):
-                a.insert(counter, temp[j])
-            counter += lt
+    for i in range(len(a)):
+        lt = len(a[i])
+        if lt > 1:
+            insertion_sort(a[i], lt)
+    return list(chain.from_iterable(a))
