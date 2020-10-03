b2e830a3702ee0271ef2a6f4a3530c44dcd22ea8
thatsOven <68780017+thatsOven@users.noreply.github.com>
2020-10-03 10:09:08 +0200

    now staticSort uses O(1) memory

--- thatsOven-s_staticSort.py
+++ thatsOven-s_staticSort.py
@@ -1,4 +1,4 @@
-# 2020 Copyright(C) thatsOven
+# Copyright(C) 2020 thatsOven
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
